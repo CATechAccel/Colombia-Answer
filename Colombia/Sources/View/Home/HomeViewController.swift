b7001011c922fc128fbfa2a6484c69c0b4a719a8
Takuma Osada <judgment9ttk069@gmail.com>
2021-03-13 23:28:09 +0900

    add available

--- Colombia/Sources/View/Home/HomeViewController.swift
+++ Colombia/Sources/View/Home/HomeViewController.swift
@@ -55,6 +55,7 @@ final class HomeViewController: UIViewController {
         super.init(nibName: nil, bundle: nil)
     }
 
+    @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
--- Colombia/Sources/View/User/UserViewController.swift
+++ Colombia/Sources/View/User/UserViewController.swift
@@ -58,6 +58,7 @@ final class UserViewController: UIViewController {
         super.init(nibName: nil, bundle: nil)
     }
 
+    @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
